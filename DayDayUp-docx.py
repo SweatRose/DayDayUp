b03501192ec484e2dd05f58a4388af2c20c4b9b2
zhz <zhz@hopesoft.com>
2023-12-20 15:24:09 +0800

    提交代码：新增企业微信推送功能|优化推送内容及word文档

--- DayDayUp-docx.py
+++ DayDayUp-docx.py
@@ -12,12 +12,17 @@ import time                                         # 时间处理
 import re                                           # 正则匹配
 import xlwt                                         # 进行excel操作
 from pyquery import PyQuery as pq
+import os
+import shutil
 #操作word的库
 from docx import Document
-from docx import Document
-from docx.shared import Pt
+from io import BytesIO
+from w3lib.html import remove_tags
 from docx.oxml.ns import qn
-from docx.shared import RGBColor
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT  # 设置对象居中、对齐等。
+from docx.shared import Inches  # 设置图像大小
+from docx.shared import Pt  # 设置像素、缩进等
+from docx.shared import RGBColor  # 设置字体颜色
 
 def main():
     datalist = []                                   # 保存文章标题和内容
@@ -26,7 +31,6 @@ def main():
     theme = input("请选择爬取内容代码（申论热点：4006、申论范文：4005、申论技巧：4007、全部申论：3994、行测技巧：3993、面试技巧：3995、面试热点：4011、公安基础知识：4012、综合指导：3997）：")
     page = input("请输入需要爬取的页数：")
     URL_list = get_pages_url(theme, int(page))              # 获取每个文章的URL
-    savepath = r'.\\' + theme_list[theme] + '.xls'
     for i in range(len(URL_list)):
         print("正在获取第{}篇范文".format(i+1))
         # datalist.extend(get_Data(URL_list[i]))
@@ -36,6 +40,8 @@ def main():
         i += 1
 
         saveData(datalist, theme_list[theme])
+        # saveData(title, content)
+
 
 def askURL(url):
     headers = {
@@ -74,26 +80,63 @@ def get_Data(url):
     content = ""
     list01 = str(bs.find_all('div', class_="offcn_shocont"))
     content = re.sub(r'<.*?>', '', list01)  # 去除HTML标签
-    content = re.sub(r'\[.*?\]', '', content)  # 去除[]标签
+    content = re.sub(r'\[.*?\]', '', content)  # 去除方括号标签
     articleData.append(str(content))
     return articleData
 
-def saveData(datalist, theme_name):  # 修改saveData函数的参数列表
+def clean_filename(filename):
+    cleaned_filename = re.sub(r'[\\/*?:"<>|]', '', filename)  # 去除文件名中的非法字符
+    cleaned_filename = cleaned_filename.replace('(进入阅读模式)', '')  # 去除标题中的"(进入阅读模式)"
+    return cleaned_filename.strip()  # 去除首尾空格
+
+def saveData(datalist, theme_name):
     for i, data in enumerate(datalist):
         doc = Document()
+        title = data[0].replace('(进入阅读模式)', '')  # 获取标题并去除"(进入阅读模式)"
+        title = re.sub('进入阅读模式', '', title)  # 删除标题中的特殊字符
+        title = re.sub('([^\u4e00-\u9fa5\d])', '', title)  # 删除标题中的特殊字符
         title_paragraph = doc.add_paragraph()
-        title_run = title_paragraph.add_run(data[0].replace('(进入阅读模式)', ''))
-        title_run = title_paragraph.add_run(data[0])
+        title_run = title_paragraph.add_run(title)
         title_run.font.name = '微软雅黑'
-        title_run.font.size = Pt(16)
+        title_run.font.size = Pt(24)  # 设置标题大小
+        title_run.font.color.rgb = RGBColor(0, 0, 0)  # 字体颜色
+        title_paragraph.alignment = 1  # 设置标题居中
+        
 
+
+        content = data[1].replace('[', '').replace(']', '')  # 去除正文中的方括号及其中的内容
+        content = re.sub('进入阅读模式', '', content)  # 删除标题中的特殊字符
+        # content = re.sub(r'&[gl]t;.*?\|.*?元.*?核心考点', '', content)
+        # content = re.sub(r'&gt;.*?$', '', content, flags=re.MULTILINE)
         content_paragraph = doc.add_paragraph()
-        content_run = content_paragraph.add_run(data[1])
+        content_run = content_paragraph.add_run(content)
         content_run.font.name = '仿宋'
-        content_run.font.size = Pt(12)
+        content_run.font.size = Pt(16)
 
-        doc.save('./archive/' + data[0] +'.docx')
+        # 添加图片
+        try:
+            image_urls = data[2]  # 假设data[2]是图片的URL列表
+            for img_url in image_urls:
+                response = requests.get(img_url)
+                image_stream = BytesIO(response.content)
+                doc.add_picture(image_stream, width=Inches(4))  # 设置图片宽度为4英寸
+        except IndexError:
+            pass  # 如果没有图片URL列表，跳过添加图片的操作
+        
+
+        doc.save('./archive/' + theme_name + '-' + clean_filename(title)  +'.docx')  # 使用去除"(进入阅读模式)"后的标题作为文件名保存
+
 
 if __name__ == '__main__':
+    folder = './archive'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
     main()
     print("爬取完毕！！！")
---
+++ pusher.py
@@ -0,0 +1,102 @@
+import os
+import glob
+import requests
+from datetime import datetime
+from datetime import datetime
+from docx import Document
+import random
+
+# # 获取最新的 Word 文档
+# def get_latest_word_document():
+#     list_of_files = glob.glob('.archive/*.docx')  # 获取当前目录下所有的 .docx 文件
+#     print(list_of_files)
+#     latest_file = max(list_of_files, key=os.path.getctime)  # 获取最新的文件
+#     return latest_file
+# 获取最新的 Word 文档
+# def get_latest_word_document(directory):
+#     list_of_files = glob.glob(os.path.join(directory, '*.docx'))  # 获取指定目录下所有的 .docx 文件
+#     if list_of_files:
+#         latest_file = max(list_of_files, key=os.path.getctime)  # 获取最新的文件
+#         return latest_file
+#     else:
+#         return None
+def get_latest_word_document():
+    # 假设你想在当前目录及其子目录中随机的 一篇Word 文档
+    files = glob.glob('**/*.docx', recursive=True)
+    if files:
+        return random.choice(files)
+    else:
+        return None
+
+
+def read_docx_content(file_path):
+    doc = Document(file_path)
+    full_text = []
+    for para in doc.paragraphs:
+        full_text.append(para.text)
+    return '\n'.join(full_text)
+
+def push_to_enterprise_wechat(docx_file_path, agent_id, corp_id, corp_secret, media_id):
+    # 上传 Word 文档到企业微信
+    # 这里需要使用企业微信提供的接口进行文章的上传和推送
+    # 需要使用到企业微信的开发者权限，包括 agent_id、corp_id 和 corp_secret
+    # 以下代码仅作为示例，实际使用时需要将以下代码与企业微信的接口进行对接
+
+    #获取content内容 来自最新的word内容
+    content = read_docx_content(docx_file_path)
+    dt = datetime.now()
+    date = dt.strftime('%Y-%m-%d')
+    time = dt.strftime('%H:%M:%S')
+    file_name = os.path.basename(docx_file_path).split('.')[0]
+    push_url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(get_access_token(corp_id, corp_secret))
+    data = {
+        'touser': '@all',
+        'agentid': agent_id,
+        'msgtype': 'mpnews',
+        'mpnews': {
+            'articles': [{
+                # 'title': f'每日一练 - {file_name}- {date}',
+                'title': f'每日一练 - {file_name}',
+                #写不动了  直接写死一张图片(三天一更新 暂时没想到好办法)
+                'thumb_media_id': '3Dmx7S_K6YtJ8qIGKE9MrxxyR5k8PsabPR7ZIf4j1FP4_5A0-TLabKkZ5sGmvz38L',  # 使用上传文件后得到的 media_id 作为缩略图
+                'author': 'Tonyz',
+                'digest': f'{date}-{time} - 每日一练推送',
+                'content': content
+                }
+            ]
+        }
+    }
+    response = requests.post(push_url, json=data)
+    if response.json()['errcode'] == 0:
+        print("文章推送成功")
+    else:
+        print("文章推送失败")
+        print(response.json())
+
+
+
+# 获取 access_token
+def get_access_token(corp_id, corp_secret):
+    access_token_url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}".format(corp_id, corp_secret)
+    response = requests.get(access_token_url)
+    return response.json()['access_token']
+
+if __name__ == "__main__":
+    agent_id = "1000002"  # 替换为企业微信应用的 agent_id
+    corp_id = "ww60ea66699fe5c551"  # 替换为企业微信的 corp_id
+    corp_secret = "AGAEKr4HsQFbBsb4YAyp8BaoLGVocRQ3f8x-h48pWGY"  # 替换为企业微信的 corp_secret
+    # directory_path = "./archive"  # 替换为实际的目录路径
+    latest_docx_file = get_latest_word_document()
+    if latest_docx_file:
+        upload_url = "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token={}&type=file".format(get_access_token(corp_id, corp_secret))
+        files = {'file': open(latest_docx_file, 'rb')}
+        response = requests.post(upload_url, files=files)
+        result = response.json()
+        if result['errcode'] == 0:
+            media_id = result['media_id']
+            push_to_enterprise_wechat(latest_docx_file, agent_id, corp_id, corp_secret, media_id)
+        else:
+            print("文件上传失败")
+    else:
+        print("未找到任何 .docx 文件")
+    # push_to_enterprise_wechat(latest_docx_file, agent_id, corp_id, corp_secret)
